eed141ab1552a3eabb88ec92b9e66a8c3e13638e
Josh Wells <josh@parall.ax>
2022-12-01 21:46:28 +0000

    Solution 1-2

--- 1.ts
+++ 1.ts
@@ -4,10 +4,11 @@ import { loadInput } from "./utils.ts"
 
 const input = await loadInput(1)
 
-const totalCaloriesPerElf = input.trim()
+export const totalCaloriesPerElf = input.trim()
   .split('\n\n')
   .map(v => v.split('\n').map(v => +v))
   .map(v => v.reduce((a, b) => a + b))
   .sort((a, b) => b - a)
 
 console.log(totalCaloriesPerElf[0])
+console.log(totalCaloriesPerElf.slice(0, 3).reduce((a, b) => a + b))
