600dfe4f9ecf898007574f4b6bc8d7acaa4c675b
Josh Wells <josh@parall.ax>
2022-12-03 21:46:08 +0000

    Solution 3-2

--- 3.ts
+++ 3.ts
@@ -1,24 +1,28 @@
 #!/usr/bin/env -S deno run --allow-read=input
 
 import { distinct } from 'std/collections/distinct.ts'
+import { intersect } from 'std/collections/intersect.ts'
+import { chunk } from 'std/collections/chunk.ts'
 import { loadInput } from './utils.ts'
 
-const input = await loadInput(3)
-const rucksacks = input.trim()
-  .split('\n')
-  .map(r => [r.substring(0, r.length / 2), r.substring(r.length / 2)])
-
-const commonItemTypes = rucksacks.map(r => r.map(c => distinct(c.split('')).sort()))
-  .map(([a, b]) => {
-    for (const i of a) {
-      if (b.includes(i)) return i
-    }
-    throw new Error(`Failed to find common item between ${a.join('')} and ${b.join('')}`)
-  })
-
+const normaliseRucksack = (r: string) => distinct(r.split('')).sort()
 const prioritise = (itemType: string) => {
   const upper = itemType === itemType.toUpperCase()
   return itemType.toUpperCase().charCodeAt(0) - 64 + (upper ? 26 : 0)
 }
 
-console.log(commonItemTypes.map(prioritise).reduce((a, b) => a + b))
+const input = await loadInput(3)
+const rucksacks = input.trim()
+  .split('\n')
+
+const compartments = rucksacks.map(r => [r.substring(0, r.length / 2), r.substring(r.length / 2)])
+const commonItemTypePerRucksack = compartments.map(r => r.map(normaliseRucksack))
+  .map(([a, b]) => intersect(a, b))
+  .map(([a]) => a)
+
+const groupedRucksacks = chunk(rucksacks.map(normaliseRucksack), 3)
+const badgePerGroup = groupedRucksacks.map(([a, b, c]) => intersect(a, b, c))
+  .map(([a]) => a)
+
+console.log(commonItemTypePerRucksack.map(prioritise).reduce((a, b) => a + b))
+console.log(badgePerGroup.map(prioritise).reduce((a, b) => a + b))
