40684d057f4080476183f0b5c6eda9d32aaf2fb3
Josh Wells <josh@parall.ax>
2022-12-06 00:02:15 +0000

    Solution 5-2

--- 5.ts
+++ 5.ts
@@ -6,19 +6,23 @@ const input = await loadInput(5)
 const [stacksInput, procedureInput] = input.trim().split('\n\n')
 
 // Parse initial stack state
-const stackLines = stacksInput.split('\n')
-const stackIdLine = stackLines.pop()
-if (!stackIdLine) throw new Error('Bad input: empty file')
+const parseStacks = (rawStack: string) => {
+  const stackLines = rawStack.split('\n')
+  const stackIdLine = stackLines.pop()
+  if (!stackIdLine) throw new Error('Bad input: empty file')
 
-const stackCount = ((stackIdLine.length - 2) / 4) + 1
-const stacks: Record<string, string[]> = Array.from(Array(stackCount))
+  const stackCount = ((stackIdLine.length - 2) / 4) + 1
+  const stacks: Record<string, string[]> = Array.from(Array(stackCount))
   .reduce((s, _, i) => ({ ...s, [i + 1 + '']: [] }), {})
 
-for (const line of stackLines) {
-  for (let i = 0; i < stackCount; i++) {
-    const maybeChar = line.charAt((i * 4) + 1)
-    if (/[a-z]/i.test(maybeChar)) stacks[i + 1].unshift(maybeChar)
+  for (const line of stackLines) {
+    for (let i = 0; i < stackCount; i++) {
+      const maybeChar = line.charAt((i * 4) + 1)
+      if (/[a-z]/i.test(maybeChar)) stacks[i + 1].unshift(maybeChar)
+    }
   }
+
+  return stacks
 }
 
 // Parse crane procedure
@@ -30,14 +34,25 @@ const procedure = procedureInput.split('\n').map(l => {
   return { src, dest, count: +count }
 })
 
-// Apply procedure
+// Apply procedure - part one version
+const partOneStacks = parseStacks(stacksInput)
 for (const { src, dest, count } of procedure) {
   for (let i = 0; i < count; i++) {
-    const item = stacks[src].pop()
+    const item = partOneStacks[src].pop()
     if (!item) throw new Error('Popped from empty stack')
-    stacks[dest].push(item)
+    partOneStacks[dest].push(item)
   }
 }
 
+// Apply procedure - part two version
+const partTwoStacks = parseStacks(stacksInput)
+for (const { src, dest, count } of procedure) {
+  const items = partTwoStacks[src].slice(-count)
+  if (items.length !== count) throw new Error('Attempted to move too many items')
+  partTwoStacks[src] = partTwoStacks[src].slice(0, -count)
+  partTwoStacks[dest] = partTwoStacks[dest].concat(items)
+}
+
 // Extract answer
-console.log(Object.values(stacks).map(s => s[s.length - 1]).join(''))
+console.log(Object.values(partOneStacks).map(s => s[s.length - 1]).join(''))
+console.log(Object.values(partTwoStacks).map(s => s[s.length - 1]).join(''))
