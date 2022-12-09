289cca44c3eaa63c3139536ea98e42171f42683d
Josh Wells <josh@parall.ax>
2022-12-09 23:04:58 +0000

    Reduced repetition in solution 8-1
    Pretty sure this makes it's less clear what's happening though

--- 8.ts
+++ 8.ts
@@ -2,6 +2,8 @@
 
 import { loadInput } from './utils.ts'
 
+type Axis = 'x' | 'y'
+type Change = 'increase' | 'decrease'
 type Tree = {
   height: number
   visible: boolean
@@ -14,68 +16,38 @@ const trees: Tree[][] = input.trim()
   .map(l => l.split('').map(c => ({ height: +c, visible: false })))
 const max = trees.length - 1
 
-// Look right
-for (let y = 0; y <= max; y++) {
-  const edgeTree = trees[y][0]
-  let maxHeight = edgeTree.height
-  edgeTree.visible = true
+const look = (axis: Axis, change: Change) => {
+  const initial = change === 'increase' ? 1 : max - 1
+  const condition: (v: number) => boolean = change === 'increase'
+    ? v => v <= max
+    : v => v >= 0
+  const afterthought: (v: number) => number = change === 'increase'
+    ? v => v + 1
+    : v => v - 1
 
-  for (let x = 1; x <= max; x++) {
-    const currentTree = trees[y][x]
+  for (let i = 0; i <= max; i++) {
+    const edgeTree = axis === 'x'
+      ? trees[i][change === 'increase' ? 0 : max]
+      : trees[change === 'increase' ? 0 : max][i]
+    let maxHeight = edgeTree.height
+    edgeTree.visible = true
 
-    if (currentTree.height > maxHeight) {
-      maxHeight = currentTree.height
-      currentTree.visible = true
+    for (let j = initial; condition(j); j = afterthought(j)) {
+      const currentTree = axis === 'x'
+        ? trees[i][j]
+        : trees[j][i]
+
+      if (currentTree.height > maxHeight) {
+        maxHeight = currentTree.height
+        currentTree.visible = true
+      }
     }
   }
 }
 
-// Look left
-for (let y = 0; y <= max; y++) {
-  const edgeTree = trees[y][max]
-  let maxHeight = edgeTree.height
-  edgeTree.visible = true
-
-  for (let x = max - 1; x >= 0; x--) {
-    const currentTree = trees[y][x]
-
-    if (currentTree.height > maxHeight) {
-      maxHeight = currentTree.height
-      currentTree.visible = true
-    }
-  }
-}
-
-// Look down
-for (let x = 0; x <= max; x++) {
-  const edgeTree = trees[0][x]
-  let maxHeight = edgeTree.height
-  edgeTree.visible = true
-
-  for (let y = 1; y <= max; y++) {
-    const currentTree = trees[y][x]
-
-    if (currentTree.height > maxHeight) {
-      maxHeight = currentTree.height
-      currentTree.visible = true
-    }
-  }
-}
-
-// Look up
-for (let x = 0; x <= max; x++) {
-  const edgeTree = trees[max][x]
-  let maxHeight = edgeTree.height
-  edgeTree.visible = true
-
-  for (let y = max - 1; y >= 0; y--) {
-    const currentTree = trees[y][x]
-
-    if (currentTree.height > maxHeight) {
-      maxHeight = currentTree.height
-      currentTree.visible = true
-    }
-  }
-}
+look('x', 'increase')
+look('x', 'decrease')
+look('y', 'increase')
+look('y', 'decrease')
 
 console.log(trees.map(line => line.filter(({ visible }) => visible).length).reduce((a, b) => a + b))
